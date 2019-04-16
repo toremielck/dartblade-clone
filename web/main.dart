8a0738ab58d80f450e52b20763090a7611cdd478
Odzga, Mario <mario.odzga@stud.th-luebeck.de>
2019-04-16 08:19:07 +0000

    Update main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,1 +1,27 @@
-Šjh®×Z®Ømš[è‰ÙšŠ{Ú®‹­¦ë
+import 'dart:html';
+
+void main() {
+  var output = document.querySelector("#output");
+  var startButton = document.querySelector("#start");
+  var game = document.querySelector("#game");
+  var qr = document.querySelector("noSmartphone");
+
+  var mobile = false;
+  startButton.onClick.listen((e){
+    output.style.display = 'none';
+    game.style.display = 'block';
+  });
+
+  window.onDeviceOrientation.listen((ev) {
+    if(ev.alpha == null && ev.beta == null && ev.gamma == null){
+      qr.style.display = 'block';
+    }
+    else{
+      qr.style.display = 'none';
+      mobile = true;
+    }
+  });
+
+
+
+}
