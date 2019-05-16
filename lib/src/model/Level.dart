d08ad4da03211c06ea5663e276167107fbc2f3f3
Tore Mielck <tore.mielck@gmail.com>
2019-05-16 13:06:54 +0200

    Positions-Felder für das Level inkl. getter & setter hinzugefügt

--- lib/src/model/Level.dart
+++ lib/src/model/Level.dart
@@ -2,4 +2,19 @@ part of modelLib;
 
 class Level{
 
+  // Position des Levels
+  int _position_x;
+  int _position_y;
+
+  int get position_x => _position_x;
+  int get position_y => _position_y;
+
+  set position_x(int value) {
+    _position_x = value;
+  }
+
+  set position_y(int value) {
+    _position_y = value;
+  }
+
 }
